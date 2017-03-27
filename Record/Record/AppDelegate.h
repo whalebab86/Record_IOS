360f93ff69df30b9bc620ba7dcaa12f049391abd
BongKi Cho <whalebab86@gmail.com>
2017-03-27 17:27:38 +0900

    init project setting

---
+++ Record/Record/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Record
+//
+//  Created by CLAY on 2017. 3. 27..
+//  Copyright © 2017년 whalebab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
