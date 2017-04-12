d4387849ee3e6dfb7ff10402996cf837b521e1cd
BongKi Cho <whalebab86@gmail.com>
2017-04-12 14:34:37 +0900

    20170412 member-diary UI Combine

---
+++ Record/Record/RCMainTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  RCMainTabBarController.h
+//  Record
+//
+//  Created by CLAY on 2017. 4. 12..
+//  Copyright © 2017년 whalebab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCMainTabBarController : UITabBarController
+
+@end
